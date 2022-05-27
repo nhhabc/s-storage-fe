bf70b523fb252f7c8684a53de717e7511ff66013
unknown <appleluvxx9x@gmail.com>
2022-05-27 16:44:05 +0700

    fix navbar

--- src/layout/MainHeader.js
+++ src/layout/MainHeader.js
@@ -3,10 +3,13 @@ import './MainHeader.scss'
 import UserService from "../services/UserService";
 import UserApi from "../api/UserApi";
 import SocketApi from "../api/SocketApi";
+import {useDispatch} from "react-redux";
+import {messageAction} from "../store/message-slice";
 
 const MainHeader = () => {
     const isLoggedIn = UserService.isAuthenticated();
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const logoutHandler = async () => {
         const res = await UserApi.getUser()
@@ -22,7 +25,7 @@ const MainHeader = () => {
             <div className='nav__logo' onClick={() => navigate('/welcome')}>S-Storage</div>
             <div className='nav__options'>
                 {isLoggedIn && <NavLink activeclassname='active' to='/folder'><p>Folder</p></NavLink>}
-                {isLoggedIn && <NavLink activeclassname='active' to='/msg'><p>Message</p></NavLink>}
+                {isLoggedIn && <NavLink activeclassname='active' to='/msg' onClick={() => dispatch(messageAction.closeMsgHandle(false))}><p>Message</p></NavLink>}
             </div>
             {!isLoggedIn && <button onClick={() => navigate('/login')}>Login</button>}
             {isLoggedIn && <button onClick={logoutHandler}>Logout</button>}
